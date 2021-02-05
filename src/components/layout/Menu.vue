c2b55a3c447310a9972f5d81386672f67db388ae
Abraão Pinto <abraao.pinto@bancoamazonia.com.br>
2021-02-05 17:57:30 -0300

    criando a base do menu da aplicação

---
+++ src/components/layout/Menu.vue
@@ -0,0 +1,40 @@
+<template>
+  <q-list>
+    <q-item-label header>Menu</q-item-label>
+    <q-item
+      clickable
+      tag="h"
+      to="/"
+    >
+      <q-item-section avatar>
+        <q-icon name="school" />
+      </q-item-section>
+      <q-item-section>
+        <q-item-label>Home</q-item-label>
+        <q-item-label caption>Pagina inicial</q-item-label>
+      </q-item-section>
+    </q-item>
+    <q-item
+      clickable
+      tag="a"
+      to="/about"
+    >
+      <q-item-section avatar>
+        <q-icon name="school" />
+      </q-item-section>
+      <q-item-section>
+        <q-item-label>Home</q-item-label>
+        <q-item-label caption>Pagina inicial</q-item-label>
+      </q-item-section>
+    </q-item>
+  </q-list>
+</template>
+
+<script>
+export default {
+  name: 'Menu'
+}
+</script>
+
+<style>
+</style>
