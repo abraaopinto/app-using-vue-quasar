71d8e0f2697fead3df1bef082af33202611503b5
Abraão Pinto <abraao.pinto@bancoamazonia.com.br>
2021-02-05 17:57:57 -0300

    criando a base do layout principal

---
+++ src/components/layout/MainLayout.vue
@@ -0,0 +1,57 @@
+<template>
+  <q-layout view="hHh lpr fFf">
+    <q-header elevated class="glossy">
+      <q-toolbar>
+        <q-btn
+          flat
+          dense
+          round
+          @click="leftDrawerOpen = !leftDrawerOpen"
+          aria-label="Menu"
+          icon="menu"
+        />
+
+        <q-toolbar-title>
+          Quasar App
+        </q-toolbar-title>
+
+        <div>Quasar v{{ $q.version }}</div>
+      </q-toolbar>
+    </q-header>
+
+    <q-drawer
+      v-model="leftDrawerOpen"
+      show-if-above
+      bordered
+      content-class="bg-grey-2"
+    >
+    <Menu />
+
+    </q-drawer>
+
+    <q-page-container>
+      <router-view />
+    </q-page-container>
+  </q-layout>
+</template>
+
+<script>
+import Menu from '@/components/layout/Menu.vue'
+
+export default {
+  name: 'MainLayout',
+
+  components: {
+    Menu
+  },
+
+  data () {
+    return {
+      leftDrawerOpen: false
+    }
+  }
+}
+</script>
+
+<style>
+</style>
