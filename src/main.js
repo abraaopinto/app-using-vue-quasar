2b3c7fda087c4b81ff52e83cd3e17ea9ccac6f25
Abraão Pinto <abraao.pinto@bancoamazonia.com.br>
2021-03-01 18:35:38 -0300

    install global vue useful

--- src/main.js
+++ src/main.js
@@ -3,8 +3,11 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import './quasar'
+import useful from 'useful-simple-functions'
 
 Vue.config.productionTip = false
+
+Vue.prototype.$useful = useful
 
 new Vue({
   router,
