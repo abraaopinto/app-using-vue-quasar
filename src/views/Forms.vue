f8a1f0718803cd646f52b3154d8d674832ac07f3
Abraão Pinto <abraao.pinto@bancoamazonia.com.br>
2021-03-01 18:39:04 -0300

    add useful sanitizeAndCapitalize

---
+++ src/views/Forms.vue
@@ -0,0 +1,37 @@
+<template>
+  <q-page padding>
+    <div>
+      <h5>Input</h5>
+      <div class="row">
+        <q-input
+          class="col-2"
+          v-model="texto"
+          label="Digite um texto"
+          clearable
+          @input="val => texto = $useful.sanitizeAndCapitalize(val)"
+          :hint="texto"
+          :rules="[val => !!val || 'O campo é obrigatorio!']"
+        />
+        <q-input
+          class="col-2"
+          v-model="texto"
+          label="Digite um texto"
+          clearable
+          type="textarea"
+          @input="val => texto = $useful.sanitizeAndCapitalize(val)"
+          :hint="texto"
+        />
+
+      </div>
+    </div>
+  </q-page>
+</template>
+<script>
+export default {
+  data () {
+    return {
+      texto: ''
+    }
+  }
+}
+</script>
